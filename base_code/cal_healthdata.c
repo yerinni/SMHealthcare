217804111966ab40c1e8e0cdf7c106ddfffa9aeb
yerinni <mintyerin0102@naver.com>
2024-12-20 20:25:28 +0900

    프로젝트 ing

--- base_code/cal_healthdata.c
+++ base_code/cal_healthdata.c
@@ -62,12 +62,18 @@ void printHealthData(const HealthData* health_data) {
 	
 	// ToCode: to print out the saved history of exercises
 	printf("=========================== History of Exercise =======================\n");
+	
+	printf("Exercise: %s, Calories burned : %d kcal\n ")
+	printf("Exercise: %s, Calories burned : %d kcal\n ")
   
   
     printf("=======================================================================\n");
 
     // ToCode: to print out the saved history of diets
     printf("============================= History of Diet =========================\n");
+    
+    printf("Food: %s, Calories intake: %d kcal\n ")
+    printf("Food: %s, Calories intake: %d kcal\n ")
 
 
     printf("=======================================================================\n");
@@ -76,6 +82,10 @@ void printHealthData(const HealthData* health_data) {
 	// ToCode: to print out the saved history of calories including basal metabolic rate, 
 	// total calories burned, total calories intake, and the remaining calories
 	printf("============================== Total Calories =========================\n");
+	printf("Basal metabolic Rate: %d kcal\n ")
+	printf("Total calories burned: %d kcal\n ")
+	printf("Total calories intake: %d kcal\n ")
+	printf("The remaining calories: %d kcal\n ")
  
  
     printf("=======================================================================\n \n");
